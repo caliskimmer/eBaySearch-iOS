2dc4dfb42019af0e9779b3666c574a6c72d49d15
caliskimmer <caliskimmer@gmail.com>
2019-05-07 13:28:22 -0700

    added sample Constants.swift file

---
+++ Constants.sample.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class Constants {
+    /* Facebook Developer API key */
+    static let FACEBOOK_API=
+
+    /* Server Path provided in server.js */
+    static let SERVER_PATH=
+
+    /* Username received after registering for Geonames API */
+    static let GEONAMES_USER=
+}
